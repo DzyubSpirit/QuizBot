41fdd4e53545571335d580999f49c93eafd08c58
Vlad Dziuba <dzyubavlad@gmail.com>
2018-03-05 19:02:42 +0200

    Implement updating scores

--- main.go
+++ main.go
@@ -4,13 +4,22 @@ import (
 	"flag"
 	"log"
 	"gopkg.in/telegram-bot-api.v4"
+	"net/url"
+	"strconv"
+	"net/http"
+	"fmt"
+	"encoding/json"
 )
 
-//421904983:AAFYGcsueGwqVX1gYSW9YcoNXntt-xxaWqo
 func main() {
 	botToken := flag.String("bot_token", "", "token of the Telegram bot")
+	gameShortName := flag.String("game_short_name", "", "short name of telegram game")
+	port := flag.String("port", "80", "port for listening score updates")
 	flag.Parse()
 	if *botToken == "" {
+		log.Fatalf("should be bot_token arg for command")
+	}
+	if *gameShortName == "" {
 		log.Fatalf("should be bot_token arg for command")
 	}
 
@@ -25,28 +34,69 @@ func main() {
 	u.Timeout = 60
 	updates, err := bot.GetUpdatesChan(u)
 	log.Println("started bot")
-	for update := range updates {
-		cq := update.CallbackQuery
-		if cq == nil {
-			continue
+	go func() {
+		for update := range updates {
+			cq := update.CallbackQuery
+			if cq == nil {
+				continue
+			}
+			if cq.GameShortName != *gameShortName {
+				continue
+			}
+			u, err := url.Parse("https://dzyubspirit.github.io/QuizBot/game.html")
+			if err != nil {
+				log.Printf("error parsing url: %v", err)
+				continue
+			}
+			q := u.Query()
+			q.Add("userId", strconv.Itoa(cq.From.ID))
+			q.Add("inlineId", cq.InlineMessageID)
+			q.Add("chatId", cq.ChatInstance)
+			q.Add("messageId", strconv.Itoa(cq.Message.MessageID))
+			u.RawQuery = q.Encode()
+			bot.AnswerCallbackQuery(tgbotapi.CallbackConfig{
+				CallbackQueryID: cq.ID,
+				URL:             u.String(),
+				Text:            "Hello, dear!",
+				ShowAlert:       true,
+			})
 		}
-		if cq.GameShortName != "test_game" {
-			continue
-		}
-		bot.AnswerCallbackQuery(tgbotapi.CallbackConfig{
-			CallbackQueryID: cq.ID,
-			URL:             "https://dzyubspirit.github.io/QuizBot/game.html",
-			Text:            "Hello, dear!",
-			ShowAlert:       true,
-		})
+	}()
 
-		/*
-			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-			msg.ReplyToMessageID = update.Message.MessageID
-
-			bot.Send(msg)
-		*/
+	type ScoreResult struct {
+		UserID    int
+		InlineID  string
+		ChatID    int
+		MessageID int
+		Score     int
 	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		var sr ScoreResult
+		err := json.NewDecoder(r.Body).Decode(&sr)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Println(w, "error parsing score result: %v", err)
+			return
+		}
+
+		cfg := tgbotapi.SetGameScoreConfig{
+			Score:  sr.Score,
+			UserID: sr.UserID,
+		}
+		if sr.InlineID != "" {
+			cfg.InlineMessageID = sr.InlineID
+		}
+		if sr.ChatID != 0 {
+			cfg.ChatID = sr.ChatID
+			cfg.MessageID = sr.MessageID
+		}
+		_, err = bot.Send(cfg)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Printf("error updating user score for scoreresult %v, err: %v", sr, err)
+			return
+		}
+		fmt.Fprint(w, "Okay")
+	})
+	log.Fatal(http.ListenAndServe(":"+*port, nil))
 }
